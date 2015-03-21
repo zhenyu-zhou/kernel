36fd11a21da03a8d6d603df73d734404e660bebd
zzy <zzy@cs.duke.edu>
2015-03-21 17:31:48 -0400

    clear dmesg

--- captcha_run.sh
+++ captcha_run.sh
@@ -1,7 +1,10 @@
 make clean
+sudo dmesg --clear
 
 g++ -c -fPIC netlink_user_lib.c  -o netlink_user_lib.o
 g++ -shared -Wl,-soname,libzzy.so -o libzzy.so netlink_user_lib.o
+
+echo "zzy: Ready to receive CAPTCHA"
 
 python render.py #> user_output_captcha
 dmesg > kernel_output_captcha
